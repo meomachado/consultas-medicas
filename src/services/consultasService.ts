7f799a80282217912bdccf3659dc4b430ffa800e
Maria Eduarda Oliveira Machado <omariaeduarda411@gmail.com>
2025-09-19 09:33:36 -0300

    fix do back

--- src/services/consultasService.ts
+++ src/services/consultasService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Consulta } from "../types/Consulta";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "http://localhost:8080"
 });
 
 export const consultasService = {
